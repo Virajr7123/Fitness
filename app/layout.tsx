010ad0143ef4366be398658521bd049e72dbcc51
Virajr7123 <105554788+Virajr7123@users.noreply.github.com>
2025-04-09 03:08:39 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Virajr7123",
+  description: "Fitness is key to sucess",
+  generator: "r7123",
 }
 
 export default function RootLayout({
